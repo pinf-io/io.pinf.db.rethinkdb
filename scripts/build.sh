fbc9774d93e3eb70a31309934f75029a8a2b50df
Christoph Dorn <christoph@christophdorn.com>
2014-06-16 17:38:56 -0700

    initial

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,26 @@
 
 # @see http://www.rethinkdb.com/docs/install/ubuntu/
 
+# NOTE: Installing using these pre-packaged packages fails way too often
+#       so we compile from source and ship our own binaries.
 
+#sudo apt-get -y install software-properties-common debian-archive-keyring
 #sudo add-apt-repository -y ppa:rethinkdb/ppa
+# TODO: Only call update if repository above was not already added.
 #sudo apt-get update
 #sudo apt-get -y install rethinkdb
 
 
+# In case above fails we can compile from source.
 # Ubuntu 13.04
-
-sudo apt-get -y install git-core g++ nodejs npm libprotobuf-dev libgoogle-perftools-dev libncurses5-dev libboost-all-dev nodejs-legacy
-
-git clone --depth 1 -b v1.12.x https://github.com/rethinkdb/rethinkdb.git
-
-cd rethinkdb
-./configure --allow-fetch
-make
+if [ -d "../../rethinkdb-1.12" ]; then
+	cp -Rf ../../rethinkdb-1.12 rethinkdb
+else
+	sudo apt-get -y install git-core g++ nodejs npm libprotobuf-dev libgoogle-perftools-dev libncurses5-dev libboost-all-dev nodejs-legacy
+	git clone --depth 1 -b v1.12.x https://github.com/rethinkdb/rethinkdb.git
+	cd rethinkdb
+	./configure --allow-fetch --fetch protobuf
+	make
+	cd ..
+	cp -Rf rethinkdb ../../rethinkdb-1.12
+fi
---
+++ scripts/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+initScript='
+description "{{=service.env.PIO_SERVICE_ID_SAFE}}"
+
+start on local-filesystems
+stop on shutdown
+
+script
+    echo $$ > {{=service.env.PIO_SERVICE_RUN_BASE_PATH}}.pid
+    # TODO: All requested environment variables should be declared dynamically.
+    export PATH={{=service.env.PATH}}
+    export PORT={{=service.env.PORT}}
+    cd {{=service.env.PIO_SERVICE_PATH}}/live/install
+    export PIO_SERVICE_DATA_BASE_PATH={{=service.env.PIO_SERVICE_DATA_BASE_PATH}}
+#    exec {{=service.env.PIO_SERVICE_PATH}}/live/install/rethinkdb/build/release/rethinkdb --bind 127.0.0.1 --http-port {{=service.env.WEB_ADMIN_PORT}} >> {{=service.env.PIO_SERVICE_LOG_BASE_PATH}}.log 2>&1
+    exec /usr/bin/rethinkdb --bind 127.0.0.1 --http-port {{=service.env.WEB_ADMIN_PORT}} >> {{=service.env.PIO_SERVICE_LOG_BASE_PATH}}.log 2>&1
+end script
+
+pre-start script
+    echo "\\n\\n['`date -u +%Y-%m-%dT%T.%3NZ`'] (/etc/init/app-{{=service.env.PIO_SERVICE_ID_SAFE}}.conf) ########## STARTING ##########\\n" >> {{=service.env.PIO_SERVICE_LOG_BASE_PATH}}.log
+end script
+
+pre-stop script
+    rm -f {{=service.env.PIO_SERVICE_RUN_BASE_PATH}}.pid
+    echo "\\n['`date -u +%Y-%m-%dT%T.%3NZ`'] (/etc/init/app-{{=service.env.PIO_SERVICE_ID_SAFE}}.conf) ^^^^^^^^^^ STOPPING ^^^^^^^^^^\\n\\n" >> {{=service.env.PIO_SERVICE_LOG_BASE_PATH}}.log
+end script
+'
+if [ -f "/etc/init/app-$PIO_SERVICE_ID_SAFE.conf" ]; then
+    sudo stop app-$PIO_SERVICE_ID_SAFE || true
+fi
+echo "$initScript" | sudo tee /etc/init/app-$PIO_SERVICE_ID_SAFE.conf
+sudo start app-$PIO_SERVICE_ID_SAFE
