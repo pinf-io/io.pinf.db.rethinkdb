0d7959225dbd682f2fd451a21383fed5746ee7cf
Christoph Dorn <christoph@christophdorn.com>
2014-10-28 21:24:34 -0700

    find existing build better

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,11 @@
 # In case above fails we can compile from source.
 # Ubuntu 13.04
 if [ -d "../../rethinkdb-1.13" ]; then
+	echo "Using existign build from $(cd ../../rethinkdb-1.13; pwd)"
 	cp -Rf ../../rethinkdb-1.13 rethinkdb
+elif [ -d "../../../built/rethinkdb-1.13" ]; then
+	echo "Using existign build from $(cd ../../../built/rethinkdb-1.13; pwd)"
+	cp -Rf ../../../built/rethinkdb-1.13 rethinkdb
 else
 	sudo apt-get update
 	sudo apt-get -y install git-core g++ nodejs npm libprotobuf-dev libgoogle-perftools-dev libncurses5-dev libboost-all-dev nodejs-legacy autogen pkg-config libcurl4-openssl-dev
